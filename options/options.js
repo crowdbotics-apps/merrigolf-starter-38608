55a7dcd523e8902258621aecc15bd30f1c0fc123
Crowdbotics <team@crowdbotics.com>
2023-01-31 19:14:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "merrigolf-starter-38608",
+  url: "https://merrigolf-starter-38608.botics.co",
+  api: "https://merrigolf-starter-38608.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
